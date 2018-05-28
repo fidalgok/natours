cb3dc63fbfaaca484df6566c15b7ea46df0eb960
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2018-05-28 14:11:24 -0400

    feature: webpack config and BEM update

---
+++ Natours/starter/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = env => ({
+  mode: env.mode,
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [miniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash:base64:7].[ext]',
+              outputPath: 'images/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new cleanWebpackPlugin(['dist']),
+    new htmlWebpackPlugin({
+      template: './index.html',
+      favicon: './img/favicon.png',
+    }),
+    new miniCssExtractPlugin(),
+  ],
+});
